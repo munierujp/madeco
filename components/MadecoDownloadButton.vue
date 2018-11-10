e77cee7673565bf5148d8c5fb4184410628d6d6a
Munieru <20086673+munierujp@users.noreply.github.com>
2018-11-11 00:19:48 +0900

    Add MadecoDownloadButton component

---
+++ components/MadecoDownloadButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-btn
+    :color="color"
+    :href="href"
+    :to="to"
+    dark>
+    <v-icon
+      left
+      dark>cloud_download</v-icon>
+    {{ $t('DOWNLOAD') }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      required: true
+    },
+    href: {
+      type: String,
+      default: ''
+    },
+    to: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
