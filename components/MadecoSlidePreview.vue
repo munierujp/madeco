86b52a0402e078a1664eb370a871c74345c25cbe
Munieru <20086673+munierujp@users.noreply.github.com>
2018-11-11 23:01:47 +0900

    Add MadecoSlidePreview component

---
+++ components/MadecoSlidePreview.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-img
+    :src="src"
+    :width="width"
+    :max-width="maxWidth"
+    :height="height"
+    :max-height="maxHeight"
+    class="madeco-slide-preview"/>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: [Number, String],
+      default: ''
+    },
+    maxWidth: {
+      type: [Number, String],
+      default: ''
+    },
+    height: {
+      type: [Number, String],
+      default: ''
+    },
+    maxHeight: {
+      type: [Number, String],
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.madeco-slide-preview {
+  border-radius: 8px;
+}
+</style>
